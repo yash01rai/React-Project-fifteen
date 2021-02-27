86c01257aae30adc02a3a805f4b35e1812a8cce0
yash01rai <yash.rai603@gmail.com>
2021-02-27 12:52:08 +0530

    Project 15

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+
+// import components
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/cocktail/:id" component={SingleCocktail} />
+        <Route path="*" component={Error} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae cum
+        velit vero id tempora iusto blanditiis, molestiae quaerat fugiat,
+        accusamus ratione facilis beatae neque doloremque ex non in voluptas
+        esse, quidem nemo asperiores. Libero iure sequi sed dicta cupiditate
+        eveniet quo porro maiores quidem delectus, voluptatem eius ipsa
+        obcaecati adipisci in nulla architecto reprehenderit qui cumque
+        distinctio ratione accusamus dolorem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
